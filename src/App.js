08365e2e17c988f54e755c369c78338d0ba4e6d9
Arthur Barcala <arthurfbarcalars@gmail.com>
2022-12-21 09:28:23 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [input, setInput] = useState("")
+
+  function handleSearch(){
+    var cep = input.replace("-", "")
+    cep = cep.replace(".", "")
+    cep = cep.replace("/", "")
+    
+    fetch("https://viacep.com.br/ws/"+cep+"/json/")
+      .then(response =>{
+        response.json()
+          .then(data => {
+            document.querySelector(".main h2").innerText = `CEP: ${data.cep}`
+            document.querySelector(".main :nth-child(2)").innerText = `Logradouro: ${data.logradouro}`
+            document.querySelector(".main :nth-child(3)").innerText = `Complemento: ${data.complemento}`
+            document.querySelector(".main :nth-child(4)").innerText = `Bairro: ${data.bairro}`
+            document.querySelector(".main :nth-child(5)").innerText = `Localidade: ${data.localidade}`
+            document.querySelector(".main").style.display = "flex"
+          })
+      })
+      .catch(error => {
+        console.error(error)
+        document.querySelector(".main").style.display = "none"
+      })
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title">Busca CEP</h1>
+
+      <div className="containerInput">
+        <input type="text"
+        placeholder="Digite seu cep..."
+        value={input}
+        onChange={(e)=>{
+          setInput(e.target.value)
+        }}/>
+
+      <button className="btnSearch" onClick={handleSearch}>🔍</button>
+      </div>
+
+      <main className="main">
+        <h2></h2>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </main>
+
     </div>
   );
 }
